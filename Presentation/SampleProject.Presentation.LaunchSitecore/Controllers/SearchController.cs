b2591cae17b8aa496e7200c4087adfa2e5a4face
Alistair Berry <aberry273@gmail.com>
2016-03-04 10:02:17 +1100

    Initial import of all projects. LaunchSitecore moved into presentation.web.LaunchSitecore project

---
+++ Presentation/SampleProject.Presentation.LaunchSitecore/Controllers/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SampleProject.Presentation.LaunchSitecore.Configuration.SiteUI;
+using SampleProject.Presentation.LaunchSitecore.Models;
+using SampleProject.Presentation.LaunchSitecore.Configuration.SiteUI.Analytics;
+using Sitecore.Analytics;
+using SampleProject.Presentation.LaunchSitecore.Configuration;
+
+namespace SampleProject.Presentation.LaunchSitecore.Controllers
+{
+  public class SearchController : LaunchSitecoreBaseController
+  {
+    [HttpGet]
+    public ActionResult Search(string searchStr, string tag)
+    {
+      return View(new SearchResults("*", new string[] { String.Format("{0}|{1}", SiteConfiguration.GetDictionaryText("Tags"), tag) }));
+    }
+
+    [HttpPost]
+    public ActionResult Search(string searchStr, string[] facets)
+    {
+      return View(new SearchResults(searchStr, facets));
+    }
+  }
+}
---
+++ Presentation/SampleProject.Presentation.LaunchSitecore/Controllers/VisitController.cs
@@ -0,0 +1,27 @@
+﻿using SampleProject.Presentation.LaunchSitecore.Configuration;
+using SampleProject.Presentation.LaunchSitecore.Configuration.SiteUI;
+using SampleProject.Presentation.LaunchSitecore.Models;
+using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleProject.Presentation.LaunchSitecore.Controllers
+{
+  public class VisitController : LaunchSitecoreBaseController
+  {
+    public ActionResult VisitDetails()
+    {
+      /* Run the query and show the same view as IconAndTitleList */
+      //VisitInformation visit = new VisitInformation();
+      return Sitecore.Context.PageMode.IsNormal ? View("VisitDetails", new VisitInformation()) : ShowEditorAlert();
+    }
+
+    private ActionResult ShowEditorAlert()
+    {
+      return View("ShowPageEditorAlert", new PageEditorAlert(PageEditorAlert.Alerts.VisitDetailsNotAllowedInPageEditor));
+    }
+  }
+}
